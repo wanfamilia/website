4a7d02e7d98e7da4e6945668f3bde4e0285d2f6e
Richard Wan <richard.wan@gettyimages.com>
2019-12-10 14:52:17 -0700

    active menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,30 @@
 class ApplicationController < ActionController::Base
-  def index
+
+  def homepage
+    coming_soon
   end
   
-  def status
-    render plain: 'Environment: ' + Rails.env.to_s
+  def our_clients
+    coming_soon
+  end
+  
+  def careers
+    coming_soon
+  end
+
+  def contact_us
+    coming_soon
+  end
+  
+  def about_us
+    info = {env: Rails.env.to_s, gitsha: `git rev-parse HEAD`.strip, action_name: action_name}
+    render inline: info.to_s, layout: 'application'
+  end
+  
+  private
+  
+  def coming_soon
+    msg = "Coming Soon: #{action_name.titleize}"
+    render inline: msg, layout: 'application'
   end
 end
